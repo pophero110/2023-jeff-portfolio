e5cff17e71a3e53eb1d473388c5f50f4f17d21dd
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-22 21:56:16 -0500

    Create a HeadShot component

---
+++ src/components/headshot/headshot.tsx
@@ -0,0 +1,15 @@
+import styles from "./headshot.module.scss";
+import Image from "next/image";
+export default function HeadShot() {
+  return (
+    <div className={styles.headShot}>
+      <img
+        src={"./image/my-headshot-compressed.png"}
+        alt="my headshot"
+        className={styles.headShot__image}
+      ></img>
+      <div className={styles.headShot__background}></div>
+      <div className={styles.headShot__borderBox}></div>
+    </div>
+  );
+}
