5e495038a911b9e3d4751f839a70908f09242cc1
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-24 20:50:33 -0500

    Create a AboutMeSection component

---
+++ src/components/aboutMeSection/aboutMeSection.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../sectionTitle/sectionTitle";
+import AboutMeContent from "./aboutMeContent/aboutMeContent";
+import styles from "./aboutMeSection.module.scss";
+export default function AboutMeSection() {
+  return (
+    <section id="about" className={styles.aboutMe}>
+      <SectionTitle title="About Me"></SectionTitle>
+      <AboutMeContent></AboutMeContent>
+    </section>
+  );
+}
