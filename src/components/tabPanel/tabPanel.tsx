cb639b900f4e2c22cd6a545f02def0c912940040
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-27 10:14:45 -0500

    Create TabList and TabPanel component

---
+++ src/components/tabPanel/tabPanel.tsx
@@ -0,0 +1,28 @@
+import styles from "./tabPanel.module.scss";
+import { JobDetail } from "../../data/jobDetail";
+export default function TabPanel({ jobDetail }: { jobDetail: JobDetail }) {
+  return (
+    <div className={styles.tabPanel}>
+      <h3 className={styles.tabPanel__title}>
+        {jobDetail.position}{" "}
+        <span className="text-highlight">
+          @{" "}
+          <a
+            href="https://www.chowbus.com"
+            target="_blank"
+            className="text-hover-underline"
+          >
+            {" "}
+            {jobDetail.company}{" "}
+          </a>
+        </span>
+      </h3>
+      <p className={styles.tabPanel__date}>{jobDetail.workPeriod}</p>
+      <ul className={styles.tabPanel__list}>
+        {jobDetail.bulletPoints.map((bulletPoint, id) => {
+          return <li key={id}>{bulletPoint}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
