8d3da37a4e30011d4cd15f835ee1786622f08785
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-28 18:28:59 -0500

    add social link data

--- src/components/sideElement/socialLink/socialLink.tsx
+++ src/components/sideElement/socialLink/socialLink.tsx
@@ -1,15 +1,20 @@
 import styles from "./socialLink.module.scss";
 import { Linkedin, GitHub, Twitter } from "./socialIcons";
+import { socialLink } from "@/data/socialLink";
 export default function SocialLink() {
   return (
     <div className={styles.socialLink}>
-      <a href="">
+      <a href={socialLink.linkedin} target="_blank">
         <Linkedin></Linkedin>
       </a>
-      <a href="">
+      <a href={socialLink.github} target="_blank">
         <GitHub></GitHub>
       </a>
-      <a href="" className={styles.socialLink__twitter}>
+      <a
+        href={socialLink.twitter}
+        target="_blank"
+        className={styles.socialLink__twitter}
+      >
         <Twitter></Twitter>
       </a>
     </div>
---
+++ src/data/socialLink.ts
@@ -0,0 +1,11 @@
+interface SocialLink {
+  github: string;
+  linkedin: string;
+  twitter: string;
+}
+
+export const socialLink = {
+  github: "https://github.com/pophero110",
+  linkedin: "https://www.linkedin.com/in/jiajin-ou-87330a1b1/",
+  twitter: "https://twitter.com/oujiaijn",
+};
