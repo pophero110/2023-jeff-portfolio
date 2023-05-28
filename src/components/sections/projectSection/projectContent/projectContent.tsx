2dc8c9dfea6698617cc7e28ae201b7b5c20e2c86
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-27 20:57:27 -0500

    Display a list of projects and add layout style to ProjectContent and ProjectImage

--- src/components/sections/projectSection/projectContent/projectContent.tsx
+++ src/components/sections/projectSection/projectContent/projectContent.tsx
@@ -1,5 +1,12 @@
+import { Project } from "@/data/project";
 import styles from "./projectContent.module.scss";
-export default function ProjectContent() {
+export default function ProjectContent({
+  project,
+  onLeft,
+}: {
+  project: Project;
+  onLeft: boolean;
+}) {
   return (
     <div className={styles.project}>
       <p className={styles.project__overline}>Featured Project</p>
@@ -9,27 +16,32 @@ export default function ProjectContent() {
           rel="noopener noreferrer"
           target="_blank"
         >
-          Tic-tac-toe
+          {project.name}
         </a>
       </h3>
       <div className={styles.project__description}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor
-          pretium leo, at consequat ligula posuere sed. Donec tristique massa ac
-          arcu hendrerit, sed convallis est laoreet. Integer et neque ac enim
-          condimentum pharetra non eu ligula. Mauris varius dapibus facilisis. .
-        </p>
+        <p>{project.description}</p>
       </div>
-      <ul className={styles.project__techList}>
-        <li>JavaScript</li>
-        <li>HTML</li>
-        <li>SASS</li>
-        <li>Socket.IO</li>
-        <li>Heroku</li>
+      <ul
+        className={
+          styles.project__techList +
+          " " +
+          (onLeft ? styles.project__techList__onLeft : "")
+        }
+      >
+        {project.techList.map((techItem) => {
+          return <li key={techItem + "1"}>{techItem}</li>;
+        })}
       </ul>
-      <div className={styles.project__links}>
+      <div
+        className={
+          styles.project__links +
+          " " +
+          (onLeft ? styles.project__links__onLeft : "")
+        }
+      >
         <a
-          href="https://github.com/pophero110/Tic-tac-toe"
+          href={project.repoLink}
           aria-label="GitHub Link"
           rel="noopener noreferrer"
           target="_blank"
@@ -50,30 +62,32 @@ export default function ProjectContent() {
             <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
           </svg>
         </a>
-        <a
-          href="https://my-tic-tac-toe.herokuapp.com/"
-          aria-label="External Link"
-          rel="noopener noreferrer"
-          target="_blank"
-          className={styles.project__links__externalLink}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="text-hover-highlight"
+        {project.externalLink && (
+          <a
+            href={project.externalLink}
+            aria-label="External Link"
+            rel="noopener noreferrer"
+            target="_blank"
+            className={styles.project__links__externalLink}
           >
-            <title>External Link</title>
-            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-            <polyline points="15 3 21 3 21 9"></polyline>
-            <line x1="10" y1="14" x2="21" y2="3"></line>
-          </svg>
-        </a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="text-hover-highlight"
+            >
+              <title>External Link</title>
+              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+              <polyline points="15 3 21 3 21 9"></polyline>
+              <line x1="10" y1="14" x2="21" y2="3"></line>
+            </svg>
+          </a>
+        )}
       </div>
     </div>
   );
--- src/components/sections/projectSection/projectSection.tsx
+++ src/components/sections/projectSection/projectSection.tsx
@@ -2,18 +2,43 @@ import SectionTitle from "@/components/sectionTitle/sectionTitle";
 import styles from "./projectSection.module.scss";
 import ProjectImage from "./projectImage/projectImage";
 import ProjectContent from "./projectContent/projectContent";
+import { projects } from "@/data/project";
 export default function ProjectSection() {
   return (
     <section id="project" className={`${styles.projectSection} section`}>
       <SectionTitle title="Some Things I’ve Built"></SectionTitle>
-      <div className={styles.projectSection__wrapper}>
-        <div className={styles.projectSection__wrapper__content}>
-          <ProjectContent></ProjectContent>
-        </div>
-        <div className={styles.projectSection__wrapper__image}>
-          <ProjectImage></ProjectImage>
-        </div>
-      </div>
+      {projects.map((project, id) => {
+        return (
+          <div key={project.name} className={styles.projectSection__wrapper}>
+            <div
+              className={
+                id % 2 === 1
+                  ? styles.projectSection__wrapper__leftContent
+                  : styles.projectSection__wrapper__rightContent
+              }
+            >
+              <ProjectContent
+                project={project}
+                onLeft={id % 2 === 1}
+              ></ProjectContent>
+            </div>
+            <div
+              className={
+                id % 2 === 1
+                  ? styles.projectSection__wrapper__rightImage
+                  : styles.projectSection__wrapper__leftImage
+              }
+            >
+              <ProjectImage
+                imageUrl={project.imageUrl}
+                link={
+                  project.externalLink ? project.externalLink : project.repoLink
+                }
+              ></ProjectImage>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 }
