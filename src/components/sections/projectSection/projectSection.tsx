52e32e278df767ef24f506acc029910f7da7b7b6
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-29 11:30:26 -0500

    Create ProjectSection component

--- src/components/sections/projectSection/projectSection.tsx
+++ src/components/sections/projectSection/projectSection.tsx
@@ -1,44 +1,17 @@
-import SectionTitle from "@/components/sectionTitle/sectionTitle";
 import styles from "./projectSection.module.scss";
-import ProjectImage from "./projectImage/projectImage";
-import ProjectContent from "./projectContent/projectContent";
+import ProjectCard from "./projectCard/projectCard";
 import { projects } from "@/data/project";
 export default function ProjectSection() {
   return (
-    <section id="project" className={`${styles.projectSection} section`}>
-      <SectionTitle title="Some Things I’ve Built"></SectionTitle>
-      {projects.map((project, id) => {
-        return (
-          <div key={project.name} className={styles.projectSection__wrapper}>
-            <div
-              className={
-                id % 2 === 1
-                  ? styles.projectSection__wrapper__leftContent
-                  : styles.projectSection__wrapper__rightContent
-              }
-            >
-              <ProjectContent
-                project={project}
-                onLeft={id % 2 === 1}
-              ></ProjectContent>
-            </div>
-            <div
-              className={
-                id % 2 === 1
-                  ? styles.projectSection__wrapper__rightImage
-                  : styles.projectSection__wrapper__leftImage
-              }
-            >
-              <ProjectImage
-                imageUrl={project.imageUrl}
-                link={
-                  project.externalLink ? project.externalLink : project.repoLink
-                }
-              ></ProjectImage>
-            </div>
-          </div>
-        );
-      })}
+    <section className={styles.projectSection + " section"}>
+      <h2 className={styles.projectSection__title}>
+        Other Noteworthy Projects
+      </h2>
+      <ul className={styles.projectSection__projectList}>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project}></ProjectCard>;
+        })}
+      </ul>
     </section>
   );
 }
